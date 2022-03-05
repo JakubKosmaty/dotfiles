94f043012196669216524ad9caa0c46325db0bdb
Jakub Kosmaty <jacobkosmaty@gmail.com>
2022-03-05 12:32:24 +0100

    Add script responisble for installing dotfiles

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -26,6 +26,7 @@ $env:KUBE_EDITOR = "code -w"
 $env:PATH += ";C:\Users\Jakub\Documents\JetBrainsScripts"
 $env:JAVA_HOME = "C:\Users\Jakub\.jdks\temurin-11.0.13"
 $env:PATH += ";$env:JAVA_HOME\bin"
+$env:PYTHONIOENCODING="utf-8"
 
 # Node.js version manager
 fnm env --use-on-cd | Out-String | Invoke-Expression
@@ -34,6 +35,7 @@ fnm env --use-on-cd | Out-String | Invoke-Expression
 Set-Alias c clear
 Set-Alias g git
 Set-Alias k kubectl
+Set-Alias v vagrant
 function l {
   Get-ChildItem -Force @args
 }
---
+++ dotfiles_install.py
@@ -0,0 +1,77 @@
+import sys
+import json
+import argparse
+import platform
+from pathlib import Path
+
+
+def detect_os() -> str:
+    os = platform.system()
+    if os not in ['Windows', 'Linux']:
+        sys.exit(f'Unsupported OS: {os}')
+    return os
+
+
+def extract_os_symlinks(os: str) -> dict[str, str]:
+    with open('symlinks.json', 'r') as f:
+        data = json.load(f)
+        return data[os]
+
+
+def create_symlink(link: Path, target: Path) -> None:
+    try:
+        link.symlink_to(target)
+        print(f'Successfully created symlink: {link} --> {target}')
+    except FileExistsError as err:
+        raise err
+    except FileNotFoundError as err:
+        print(f'Could not create symlink: {err}')
+    except OSError as err:
+        print(f'OS error: {err}')
+
+
+def link(target: str, link: str, force: bool) -> None:
+    target_path = Path(target).resolve()
+    if not target_path.is_file():
+        print(f'File does not exists: {target_path}')
+        return
+
+    link_path = Path(link).expanduser()
+
+    try:
+        create_symlink(link_path, target_path)
+    except FileExistsError as err:
+        if force:
+            link_path.unlink()
+            create_symlink(link_path, target_path)
+        else:
+            print(f'Could not create symlink: {err}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        '--force',
+        action='store_true',
+        default=False,
+        help='If symlink already exists, remove it and create again'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    force: bool = args.force
+
+    os = detect_os()
+    symlinks = extract_os_symlinks(os)
+
+    any(link(k, v, force) for k, v in symlinks.items())
+
+
+if __name__ == "__main__":
+    main()
