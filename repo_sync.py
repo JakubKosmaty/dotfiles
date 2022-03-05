775f44df0900d466fef2c702edef77abd7f80706
Jakub Kosmaty <jacobkosmaty@gmail.com>
2022-03-05 12:45:37 +0100

    Auto Sync

--- repo_sync.py
+++ repo_sync.py
@@ -5,6 +5,7 @@ def main():
     subprocess.check_call(('git', 'pull'))
     subprocess.check_call(('git', 'add', '.'))
     subprocess.check_call(('git', 'commit', '-m', 'Auto Sync'))
+    subprocess.check_call(('git', 'push'))
 
 
 if __name__ == "__main__":
